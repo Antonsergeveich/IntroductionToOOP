7c495d688710f568d0943fa11f55eee4dcecd7bc
Persh_vx33@top-academy.ru <Persh_vx33@top-academy.ru>
2024-05-24 19:53:48 +0300

    lesson 1

--- IntroductionToOOP/main.cpp
+++ IntroductionToOOP/main.cpp
@@ -1,8 +1,74 @@
-#include<iostream>
+﻿#include<iostream>
 using namespace std;
 
+class Point
+{
+	double x;
+	double y;
+public:
+	double get_x()const
+	{
+		return x;
+	}
+	double get_y()const
+	{
+		return y;
+	}
+	void set_x(double x)
+	{
+		this->x = x;
+	}
+	void set_y(double y)
+	{
+		this->y = y;
+	}
+	//Methods:
+	double distance(Point other)
+	{
+		//this - этот, эта точка;
+		//other - другой, та точка;
+		double x_distance = this->x - other.x;
+		double y_distance = this->y - other.y;
+		double distance = sqrt(x_distance * x_distance + y_distance * y_distance);
+		return distance;
+	}
+};
+
+double distance(Point A, Point B)
+{
+	double x_distance = A.get_x() - B.get_x();
+	double y_distance = A.get_y() - B.get_y();
+	return sqrt(x_distance * x_distance + y_distance * y_distance);
+	//Функция sqrt возвращает квадратный корень принятого числа
+}
+
+//Создавая структуру или класс мы создаём новый тип данных
+//#define STRUCT_POINT
 void main()
 {
 	setlocale(LC_ALL, "");
+	Point A;
+	A.set_x(2);
+	A.set_y(3);
+	cout << A.get_x() << "\t" << A.get_y() << endl;
+#ifdef STRUCT_POINT
 	cout << "Hello OOP" << endl;
+	Point A{};//Объявление переменной 'A' типа 'Point'
+	//Создание объекта 'A' структуры 'Point'
+	//Создание экземпляра 'A' структуры 'Point'
+	A.x = 2;
+	A.y = 3;
+	cout << A.x << "\t" << A.y << endl;
+	Point* pA = &A; //Pointer to 'A'
+	cout << pA << endl;
+	cout << pA->x << "\t" << pA->y << endl;
+#endif // STRUCT_POINT
+	Point B;
+	B.set_x(7);
+	B.set_y(8);
+	cout << B.get_x() << "\t" << B.get_y() << endl;
+	cout << "Расстояние от точки 'A' до точки 'B': " << A.distance(B) << endl;
+	cout << "Расстояние от точки 'B' до точки 'A': " << B.distance(A) << endl;
+	cout << "Расстояние между точками 'B' и 'A': " << distance(B, A) << endl;
+	cout << "Расстояние между точками 'A' и 'B': " << distance(A, B) << endl;
 }
