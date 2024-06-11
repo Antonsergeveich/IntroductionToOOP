7a2bcfcbbbb61cf94a7182cfbea62f44ed4edfb6
Persh_vx33@top-academy.ru <Persh_vx33@top-academy.ru>
2024-06-11 16:44:02 +0300

    +

--- String/main.cpp
+++ String/main.cpp
@@ -24,6 +24,16 @@ public:
 		for (int i = 0; str[i]; i++)this->str[i] = str[i];
 		cout << "Constructor:\t\t" << this << endl;
 	}
+	String(const String& other)
+	{
+		this->size = other.size;
+		this->str = new char[size] {};
+		for (int i = 0; i < size; i++)
+		{
+			this->str[i] = other.str[i];
+		}
+		cout << "CopyConstructor:\t\t" << this << endl;
+	}
 	/*String(const char* str)
 	{
 		int size = strlen(str);
@@ -46,6 +56,11 @@ public:
 	{
 		return str;
 	}
+    char* get_str()
+	{
+		return str;
+	}
+   
 	int get_size()const
 	{
 		return size;
@@ -56,9 +71,22 @@ public:
 		cout << "Str:\t" << str << endl;
 	}
 };
+String operator + (const String& left, const String& right)
+{
+	String sum(left.get_size() + right.get_size());
+	for (int i = 0; i < left.get_size(); i++)
+	{
+		sum.get_str()[i] = left.get_str()[i];
+	}
+	for (int i = 0; i < right.get_size(); i++)
+	{
+		sum.get_str()[i + left.get_size()] = right.get_str()[i];
+	}
+	return sum;
+}
 std::ostream& operator << (std::ostream& os, const String& obj)
 {
-	return os<< obj.get_str();
+	return os << obj.get_str();
 }
 void main()
 {
@@ -69,8 +97,8 @@ void main()
 	String str2 = "World";
 	cout << str1 << endl;
 	cout << str2 << endl;
-	//String str3 = str1 + str2;
-	//cout << str3 << endl; //HelloWorld
+	String str3 = str1 + str2;
+	cout << str3 << endl; //HelloWorld
 }
 
 
