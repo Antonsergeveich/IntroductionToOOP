ad3d3d8eeb7148c60e0f71a0cf8681a89486184a
Persh_vx33@top-academy.ru <Persh_vx33@top-academy.ru>
2024-06-14 14:16:26 +0300

    str

--- String/main.cpp
+++ String/main.cpp
@@ -1,8 +1,12 @@
 ﻿#include<iostream>
 using namespace std;
 
+using std::cout;
+using std::cin;
+using std::endl;
+
 class String;
-//int strlength(const char* str);
+int strlength(const char* str);
 std::ostream& operator << (std::ostream& os, const String& obj);
 
 class String
@@ -11,15 +15,15 @@ class String
 	char* str;//Адрес строки в динамической памяти
 public:
 	//             Constructors:
-	String(int size = 80)
+	String(int size = 80) //size - параметр по умолчанию
 	{
-		this->size = size;
+		this->size = size; // c помощью size можно создать строку нужного размера
 		this->str = new char[size] {};
 		cout << "DefaultConstructor:\t" << this << endl;
 	}
 	String(const char str[])
 	{
-		this->size = strlen(str) + 1;
+		this->size = strlen(str) + 1; //strlen() - функция которая считает размер строки в байтах без '\0'
 		this->str = new char[size] {};
 		for (int i = 0; str[i]; i++)this->str[i] = str[i];
 		cout << "Constructor:\t\t" << this << endl;
@@ -52,15 +56,15 @@ public:
 	}
 
 	//             Methods:
-    const char* get_str()const
+	const char* get_str()const
 	{
 		return str;
 	}
-    char* set_str()
+	char* get_str()
 	{
 		return str;
 	}
-   
+
 	int get_size()const
 	{
 		return size;
@@ -76,11 +80,11 @@ String operator + (const String& left, const String& right)
 	String sum(left.get_size() + right.get_size() - 1);
 	for (int i = 0; i < left.get_size(); i++)
 	{
-		sum.set_str()[i] = left.get_str()[i];
+		sum.get_str()[i] = left.get_str()[i];
 	}
 	for (int i = 0; i < right.get_size(); i++)
 	{
-		sum.set_str()[i + left.get_size() - 1] = right.get_str()[i];
+		sum.get_str()[i + left.get_size() - 1] = right.get_str()[i];
 	}
 	return sum;
 }
@@ -94,14 +98,23 @@ void main()
 	String str; str.print();//Default Constructor
 	String str1 = "Hello";//Single-argument Constructor
 	String str2 = "World";
+	cout << "strlengh = " << strlength("Helloooooooo");
+	cout << endl;
 	cout << str1 << endl;
 	cout << str2 << endl;
 	String str3 = str1 + str2;
 	cout << str3 << endl; //HelloWorld
+
 }
 
-
-//int strlength(const char* str)
-//{
-//	for (int i = 0; str[i] != '\0'; i++)return i;
-//}
+int strlength(const char* str)
+{
+	int i = 0;
+	for (; str[i]; i++); //точка с запятой нужна чтобы
+	//return i возвращал значение i после того как выполниться цикл for;
+	//если return i будет в теле цикла, то i возвратит 0;
+	// str[i] - значение элемента массива по индексу;
+	// если в элементе массива есть символ (не '\0') то возвращается true => идём дальше 'i' увеличиваем на 1;
+	// если str[i] = '\0' возвращается false => return i;
+	return i;
+}
