62e1ac7c94dc2452143293b53ea48bfb47695215
Persh_vx33@top-academy.ru <Persh_vx33@top-academy.ru>
2024-06-17 11:41:12 +0300

    Matrix

--- Matrix/main.cpp
+++ Matrix/main.cpp
@@ -1,9 +1,11 @@
-#include<iostream>
+﻿#include<iostream>
 using namespace std;
 
 using std::cout;
 using std::cin;
 using std::endl;
+
+class Matrix;
 
 class Matrix
 {
@@ -19,7 +21,7 @@ public:
 	{
 		return cols;
 	}
-	Matrix(int rows, int cols)
+	Matrix( const int rows, const int cols)
 	{
 		this->rows = rows;
 		this->cols = cols;
@@ -42,10 +44,33 @@ public:
 		arr = nullptr;
 		cout << "Destructor:\t" << this << endl;
 	}
+	void FillRand()const
+	{
+		for (int i = 0; i < rows; i++)
+		{
+			for (int j = 0; j < cols; j++)
+			{
+				arr[i][j] = rand() % 100;
+			}
+		}
+	}
+	void print()const
+	{
+		for (int i = 0; i < rows; i++)
+		{
+			for (int j = 0; j < cols; j++)
+			{
+				cout << arr[i][j] << "\t";
+			}
+			cout << endl;
+		}
+	}
 };
 
 void main()
 {
 	setlocale(LC_ALL, "");
-
+	Matrix M(4, 5);
+	M.FillRand();
+	M.print();
 }
